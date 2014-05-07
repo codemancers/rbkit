c16695fc1e1780dee15a9a60d83fc83370f009aa
Hemant Kumar <gethemant@gmail.com>
2014-05-06 22:50:26 -0400

    Add a ruby client for receiving events

---
+++ experiments/rbkit_client.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+# To use this client install following gems
+# gem install msgpack
+# gem install rbczmq
+
+require 'zmq'
+require 'pp'
+require "msgpack"
+
+# PUB / SUB topology
+
+Thread.abort_on_exception = true
+
+ctx = ZMQ::Context.new
+socket = ctx.socket(:SUB)
+socket.verbose = true
+socket.subscribe("")
+socket.connect("tcp://localhost:5555")
+loop do
+  message = socket.recv
+  unpacked_message = MessagePack.unpack(message)
+  p unpacked_message
+end
