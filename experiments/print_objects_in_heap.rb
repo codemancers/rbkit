2a749833cea70d6e9d6343b746470c27129b9533
Emil Soman <emil.soman@gmail.com>
2014-07-06 19:06:43 +0530

    Add experiment to iterate over heap objects and print their classnames

---
+++ experiments/print_objects_in_heap.rb
@@ -0,0 +1,12 @@
+$:<< File.join(File.dirname(__FILE__), "../lib")
+$:<< File.join(File.dirname(__FILE__), "../ext")
+
+require 'rbkit'
+
+class Animal;end
+
+
+puts "Using .class : #{Animal.new.class}"
+puts "Using rb_obj_class : #{ObjectGraph.print_class(Animal.new)}"
+
+#ObjectGraph.print_heap_objects
--- ext/object_graph.c
+++ ext/object_graph.c
@@ -1,10 +1,26 @@
-#include "ruby/ruby.h"
-#include "ruby/debug.h"
+#include <ruby.h>
 
 static void
 root_obj_i(const char *category, VALUE obj, void *data)
 {
   fprintf(stderr, "CATEGORY: %s, OBJ: %p\n", category, (void *)obj);
+}
+
+static VALUE
+print_class(VALUE obj)
+{
+  return rb_obj_class(obj);
+}
+
+static int
+heap_obj_i(void *vstart, void *vend, size_t stride, void *data)
+{
+  VALUE v = (VALUE)vstart;
+
+  for (; v != (VALUE)vend; v += stride)
+    if (RBASIC(v)->flags)
+      fprintf(stderr, "CLASS: %p --- ", RBASIC_CLASS(v));
+  return 0;
 }
 
 struct some_shit {
@@ -17,7 +33,16 @@ static VALUE print_root_objects() {
   return Qnil;
 }
 
+static VALUE print_heap_objects() {
+  struct some_shit data;
+  rb_objspace_each_objects(heap_obj_i, &data);
+  return Qnil;
+}
+
+
 void init_object_graph(void) {
   VALUE objectGraphModule = rb_define_module("ObjectGraph");
   rb_define_module_function(objectGraphModule, "print_root_objects", print_root_objects, 0);
+  rb_define_module_function(objectGraphModule, "print_heap_objects", print_heap_objects, 0);
+  rb_define_module_function(objectGraphModule, "print_class", print_class, 1);
 }
