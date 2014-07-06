8d67c620dd110070b0fa5ec9c8cc82f9c9315da7
Emil Soman <emil.soman@gmail.com>
2014-07-06 19:33:20 +0530

    Try printing class using rb_class2name

--- experiments/print_objects_in_heap.rb
+++ experiments/print_objects_in_heap.rb
@@ -8,5 +8,6 @@ class Animal;end
 
 puts "Using .class : #{Animal.new.class}"
 puts "Using rb_obj_class : #{ObjectGraph.print_class(Animal.new)}"
+ObjectGraph.print_class_using_class2name(Animal.new)
 
 #ObjectGraph.print_heap_objects
--- ext/object_graph.c
+++ ext/object_graph.c
@@ -12,6 +12,15 @@ print_class(VALUE obj)
   return rb_obj_class(obj);
 }
 
+static void
+print_class_using_class2name(VALUE obj)
+{
+   VALUE klass = RBASIC_CLASS(obj);
+  if (!NIL_P(klass)) {
+    fprintf(stderr, "Using class2name : %s\n", rb_class2name(klass));
+  }
+}
+
 static int
 heap_obj_i(void *vstart, void *vend, size_t stride, void *data)
 {
@@ -45,4 +54,5 @@ void init_object_graph(void) {
   rb_define_module_function(objectGraphModule, "print_root_objects", print_root_objects, 0);
   rb_define_module_function(objectGraphModule, "print_heap_objects", print_heap_objects, 0);
   rb_define_module_function(objectGraphModule, "print_class", print_class, 1);
+  rb_define_module_function(objectGraphModule, "print_class_using_class2name", print_class_using_class2name, 1);
 }
