020bc2ff767d3f509f4e7cdc35457c500995376d
Emil Soman <emil.soman@gmail.com>
2014-07-07 20:32:06 +0530

    Fix experiment to print class name

--- experiments/print_objects_in_heap.rb
+++ experiments/print_objects_in_heap.rb
@@ -5,9 +5,6 @@ require 'rbkit'
 
 class Animal;end
 
-
-puts "Using .class : #{Animal.new.class}"
-puts "Using rb_obj_class : #{ObjectGraph.print_class(Animal.new)}"
 ObjectGraph.print_class_using_class2name(Animal.new)
 
-#ObjectGraph.print_heap_objects
+ObjectGraph.print_heap_objects
--- ext/object_graph.c
+++ ext/object_graph.c
@@ -6,16 +6,10 @@ root_obj_i(const char *category, VALUE obj, void *data)
   fprintf(stderr, "CATEGORY: %s, OBJ: %p\n", category, (void *)obj);
 }
 
-static VALUE
-print_class(VALUE obj)
-{
-  return rb_obj_class(obj);
-}
-
 static void
-print_class_using_class2name(VALUE obj)
+print_class_using_class2name(VALUE self, VALUE obj)
 {
-   VALUE klass = RBASIC_CLASS(obj);
+  VALUE klass = RBASIC_CLASS(obj);
   if (!NIL_P(klass)) {
     fprintf(stderr, "Using class2name : %s\n", rb_class2name(klass));
   }
@@ -25,10 +19,13 @@ static int
 heap_obj_i(void *vstart, void *vend, size_t stride, void *data)
 {
   VALUE v = (VALUE)vstart;
+  VALUE klass ;
 
   for (; v != (VALUE)vend; v += stride)
-    if (RBASIC(v)->flags)
-      fprintf(stderr, "CLASS: %p --- ", RBASIC_CLASS(v));
+    klass = RBASIC_CLASS(v);
+    if (!NIL_P(klass)) {
+      fprintf(stderr, "CLASS --- : %s\n", rb_class2name(klass));
+    }
   return 0;
 }
 
@@ -53,6 +50,5 @@ void init_object_graph(void) {
   VALUE objectGraphModule = rb_define_module("ObjectGraph");
   rb_define_module_function(objectGraphModule, "print_root_objects", print_root_objects, 0);
   rb_define_module_function(objectGraphModule, "print_heap_objects", print_heap_objects, 0);
-  rb_define_module_function(objectGraphModule, "print_class", print_class, 1);
   rb_define_module_function(objectGraphModule, "print_class_using_class2name", print_class_using_class2name, 1);
 }
