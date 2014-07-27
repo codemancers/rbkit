0b977a74ca09c5e8b5cc83b5d0a9547ddda8f49e
Hemant Kumar <gethemant@gmail.com>
2014-07-27 09:43:05 -0400

    Start adding unit testing support to rbkit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require "pry"
+require "simplecov"
+SimpleCov.start do
+  add_filter "/spec/"
+end
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
+end
