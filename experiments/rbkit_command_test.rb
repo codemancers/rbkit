f02d7069d0489c46f50a601596dc18da6516caf9
Emil Soman <emil.soman@gmail.com>
2014-08-02 05:23:03 +0530

    Test ruby client should accept server IP from user

--- experiments/rbkit_command_test.rb
+++ experiments/rbkit_command_test.rb
@@ -12,11 +12,17 @@ commands = [
   'trigger_gc'
 ]
 
+output_file = Tempfile.new('rbkit')
+puts "Writing output to file #{output_file.path}"
 ctx = ZMQ::Context.new
+
+puts "Enter IPv4 address of Rbkit server. (Blank for localhost) :"
+server_ip = gets.strip
+server_ip = "127.0.0.1" if server_ip.empty?
 
 Thread.new do
   request_socket = ctx.socket(:REQ)
-  request_socket.connect("tcp://127.0.0.1:5556")
+  request_socket.connect("tcp://#{server_ip}:5556")
   loop do
     puts "Available commands :"
     commands.each_with_index do |c, i|
@@ -34,17 +40,15 @@ end
 
 socket = ctx.socket(:SUB)
 socket.subscribe("")
-socket.connect("tcp://127.0.0.1:5555")
+socket.connect("tcp://#{server_ip}:5555")
 
-f = Tempfile.new('rbkit')
-puts "Writing output to file #{f.path}"
 begin
   loop do
     message = socket.recv
     unpacked_message = MessagePack.unpack(message)
-    f.puts unpacked_message
+    output_file.puts unpacked_message
   end
 ensure
-  f.close
-  f.unlink
+  output_file.close
+  output_file.unlink
 end
