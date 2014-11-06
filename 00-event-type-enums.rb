00cde41915de6bf739fae3aed2b2bd32f30e7e21
Yuva <yuva@codemancers.com>
2014-11-06 14:08:25 +0530

    script to convert event name to enums

---
+++ 00-event-type-enums.rb
@@ -0,0 +1,10 @@
+require 'msgpack'
+
+message = File.read("/Users/yuva/nomads/rbkit-client/tests/msgpack/objdestroyed")
+p unpacked_message = MessagePack.unpack(message)
+unpacked_message['event_type'] = 1
+
+
+File.open("/Users/yuva/nomads/rbkit-client/tests/msgpack/objdestroyed", "w") do |f|
+  f.write MessagePack.pack(unpacked_message)
+end
