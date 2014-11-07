6edc3883e0fba937ce038bc4b3c3589392e8097b
Yuva <yuva@codemancers.com>
2014-11-08 00:58:06 +0530

    add a script to convert keys to enums for objdeleted

---
+++ 11-obj-event-keys2enum.rb
@@ -0,0 +1,54 @@
+require 'msgpack'
+
+ek = {
+  "event_type" => 0,
+  "timestamp" => 1,
+  "payload" => 2,
+  "message_counter" => 9
+}
+
+
+def old2new(object)
+  {
+    3 => object['object_id'],
+    4 => object['class_name'],
+    5 => object['references'],
+    6 => object['file'],
+    7 => object['line'],
+    8 => object['size']
+  }.reject { |k, v| v.nil? }
+end
+
+
+file = "/Users/yuva/nomads/rbkit-client/tests/msgpack/objdestroyed"
+
+message = File.read(file)
+p unpacked_message = MessagePack.unpack(message)
+
+unpacked = {}
+unpacked_message.each do |k, v| # convert event-collection keys
+  unpacked[ek[k]] = v
+end
+
+p unpacked
+
+unpacked[2] = [{}]
+unpacked_message['payload'].first.each do |k, v| # convert first event keys
+  unpacked[2].first[ek[k]] = v
+end
+
+p unpacked
+
+unpacked[2].first[2] = old2new(unpacked[2].first[2])
+
+
+p unpacked
+
+# unpacked_message['payload'].first['payload'].map do |k, v|
+#   new_objects << new_object
+# end
+
+
+File.open(file, "w") do |f|
+  f.write MessagePack.pack(unpacked)
+end
